d86d083508f4e49d24b8e503bacb5aba0aec1783
Georges Benatti Jr <gb@MacBook-Pro-de-Georges.local>
2021-04-28 12:24:03 -0300

    - removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Filmes',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
